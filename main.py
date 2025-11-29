6af600e58a8cf69c6856a46b6dbfe7384b76ec9c
sruhshona2197 <sruhshona2197@gmail.com>
2025-11-29 17:09:47 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# 1-mashq
+bor_a = lambda ism: "a" in ism
+
+print(bor_a("Ruxshona"))
+print(bor_a("Olim"))
+
+
+# 2-mashq
+bosh_harf = lambda s: s.upper()
+
+print(bosh_harf("salom dunyo"))
+
+
+# 3-mashq
+farq_modul = lambda a, b: abs(a - b)
+
+print(farq_modul(10, 4))
+
+
+# 4-mashq
+tengmi = lambda a, b: a == b
+
+print(tengmi(5, 5))
+print(tengmi(3, 7))
+
+
+# 5-mashq
+butun_qism = lambda n: n // 2
+
+print(butun_qism(7))
+print(butun_qism(10))
